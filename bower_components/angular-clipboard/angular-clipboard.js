283c791c2903ec266edec48fd409848e8d1531c4
FoxUSA <jake@jakelis.com>
2018-04-05 21:03:08 -0700

    getObject code

--- bower_components/angular-clipboard/angular-clipboard.js
+++ bower_components/angular-clipboard/angular-clipboard.js
@@ -14,9 +14,13 @@ return angular.module('angular-clipboard', [])
         function createNode(text, context) {
             var node = $document[0].createElement('textarea');
             node.style.position = 'absolute';
-            node.textContent = text;
+            node.style.fontSize = '12pt';
+            node.style.border = '0';
+            node.style.padding = '0';
+            node.style.margin = '0';
             node.style.left = '-10000px';
             node.style.top = ($window.pageYOffset || $document[0].documentElement.scrollTop) + 'px';
+            node.textContent = text;
             return node;
         }
 
@@ -27,12 +31,22 @@ return angular.module('angular-clipboard', [])
 
                 var selection = $document[0].getSelection();
                 selection.removeAllRanges();
-                node.select();
 
-                if(!$document[0].execCommand('copy')) {
-                    throw('failure copy');
+                var range = document.createRange();
+                range.selectNodeContents(node);
+                selection.addRange(range);
+                // This makes it work in all desktop browsers (Chrome)
+                node.select();
+                // This makes it work on Mobile Safari
+                node.setSelectionRange(0, 999999);
+
+                try {
+                    if(!$document[0].execCommand('copy')) {
+                        throw('failure copy');
+                    }
+                } finally {
+                    selection.removeAllRanges();
                 }
-                selection.removeAllRanges();
             } finally {
                 // Reset inline style
                 $document[0].body.style.webkitUserSelect = '';
@@ -40,9 +54,14 @@ return angular.module('angular-clipboard', [])
         }
 
         function copyText(text, context) {
+            var left = $window.pageXOffset || $document[0].documentElement.scrollLeft;
+            var top = $window.pageYOffset || $document[0].documentElement.scrollTop;
+            
             var node = createNode(text, context);
             $document[0].body.appendChild(node);
             copyNode(node);
+
+            $window.scrollTo(left, top);
             $document[0].body.removeChild(node);
         }
 
